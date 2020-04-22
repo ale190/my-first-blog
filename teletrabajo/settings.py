b0edeef036e97eeb1c2015103069af2e5b5873d1
Alejandro <alejandrogimenez89@gmail.com>
2020-04-21 23:35:12 -0300

    210420

--- teletrabajo/settings.py
+++ teletrabajo/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'assets'), #Terminar siempre con una coma, ya que se trata de una tupla (tipo de lista)
-)
+#STATICFILES_DIR = (
+#    os.path.join(BASE_DIR, 'assets'), #Terminar siempre con una coma, ya que se trata de una tupla (tipo de lista)
+#)
