49c21d67b6c3fbccd0ce020f31d695bff1e27297
Alejandro <alejandrogimenez89@gmail.com>
2020-04-22 00:14:22 -0300

    x

--- tareas/views.py
+++ tareas/views.py
@@ -7,4 +7,4 @@ def tareas_lista(request):
     tareas = Tarea.objects.all()
     #response = ', '.join([str(tarea) for tarea in tareas]) 1era opción
     #return HttpResponse(response) 1era opción
-    return render(request, 'tareas/tareas_lista.html', {'tareas':tareas}) # 2da opción
+    return render(request, 'tareas/TAREAS_LISTA.html', {'tareas':tareas}) # 2da opción
